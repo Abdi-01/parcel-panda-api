85503b3fde8fd649993f54fbe6f075c09a549812
idoyudha <idowidya.yudhatama@gmail.com>
2021-07-28 23:48:50 +0700

    add user transaction action

--- routers/transactionManageRouter.js
+++ routers/transactionManageRouter.js
@@ -3,5 +3,6 @@ const { transactionManageController } = require('../controllers')
 const { readToken } = require('../config')
 
 router.get('/:limit/:offset', readToken, transactionManageController.getTransaction)
+router.patch('/action/:id', readToken, transactionManageController.transactionAction)
 
 module.exports = router
